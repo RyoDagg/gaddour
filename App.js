16e487902289b904f6a8005659d98957681a3cee
Gaddour <mannai.abdlkader@gmail.com>
2024-04-20 09:56:00 +0100

    counter init

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, Button, View, Pressable } from "react-native";
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{counter}</Text>
+      <View style={styles.button1}>
+        <Pressable
+          android_ripple={{ color: "#0C317830" }}
+          onPress={() => setCounter(counter + 1)}
+        >
+          <Text style={styles.text}>Next</Text>
+        </Pressable>
+      </View>
+      <View style={styles.button2}>
+        <Pressable
+          android_ripple={{ color: "#1042A2" }}
+          onPress={() => setCounter(counter - 1)}
+        >
+          <Text style={[styles.text, { color: "white" }]}>Finish</Text>
+        </Pressable>
+      </View>
+      <Button
+        color={"black"}
+        title="Reset"
+        onPress={() => {
+          setCounter(0);
+          console.log("reset");
+        }}
+      ></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +39,38 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    gap: 10,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button1: {
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 50,
+    borderColor: "#0C3178",
+    borderWidth: 2,
+    elevation: 3,
+    backgroundColor: "#fff",
+    overflow: "hidden",
+  },
+  button2: {
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 50,
+    elevation: 3,
+    backgroundColor: "#0C3178",
+    overflow: "hidden",
+  },
+  text: {
+    paddingVertical: 8 * 2,
+    paddingHorizontal: 40,
+    fontSize: 20,
+    color: "white",
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "#0C3178",
   },
 });
